a4e1150a9fbcc55ba7554c5c359b1f3dc6b078a2
Tapojyoti Mandal <mandaltj@gmail.com>
2020-02-27 00:54:27 -0600

    3darray using malloc added

--- 3darray.cpp
+++ 3darray.cpp
@@ -1,45 +1,65 @@
 #include <iostream>
 
 int main(){
-	int x_dim = 4;
-	int y_dim = 4;
-	int z_dim = 3;
+    int x_dim = 4;
+    int y_dim = 4;
+    int z_dim = 3;
 
-	int ***myarr = new int**[x_dim];
-	for(int i=0; i<x_dim; i++){
-		myarr[i] = new int*[y_dim];
-		for(int j=0; j<y_dim; j++){
-			// std::cout<<"Here!!\n";
-			myarr[i][j] = new int[z_dim];
-		}
-	}
+    int ***myarr = new int**[x_dim];
+    for(int i=0; i<x_dim; i++){
+        myarr[i] = new int*[y_dim];
+        for(int j=0; j<y_dim; j++){
+            // std::cout<<"Here!!\n";
+            myarr[i][j] = new int[z_dim];
+        }
+    }
 
-	//myarr is a pointer to an array of pointer to pointer of length x_dim
-	//myarr[i] is a pointer to an array of pointer of length y_dim
-	//myarr[i][j] is a pointer to an array of integers of length z_dim
+    int ***test_arr = (int ***)malloc(x_dim*sizeof(int**));
+    for(int i=0; i<x_dim; i++){
+        test_arr[i] = (int **)malloc(y_dim*sizeof(int *));
+        for(int j=0; j<y_dim; j++){
+            // std::cout<<"Here!!\n";
+            test_arr[i][j] = (int *)malloc(z_dim*sizeof(int));
+        }
+    }
+    //myarr is a pointer to an array of pointer to pointer of length x_dim
 
-	std::cout<<"Display 3D Array: \n";
-	for(int i=0; i<x_dim; i++){
-		std::cout<<"[";
-		for(int j=0; j<y_dim; j++){
-			std::cout<<"[";
-			for(int k=0; k<z_dim; k++){
-				std::cout<<myarr[i][j][k]<<",";
-			}
-			std::cout<<"], ";
-		}
-		std::cout<<"]\n";
-	}
+    //myarr[i] is a pointer to an array of pointer of length y_dim
+    //myarr[i][j] is a pointer to an array of integers of length z_dim
 
-	//We can't simply use delete[] myarr to delete this 3D array.
-	for(int i=0; i<x_dim; i++){
-		for(int j=0; j<y_dim; j++){
-			// std::cout<<"Here!!\n";
-			delete [] myarr[i][j];
-		}
-		delete [] myarr[i];
-	}
-	delete [] myarr;
+    std::cout<<"Display 3D Array: \n";
+    for(int i=0; i<x_dim; i++){
+        std::cout<<"[";
+        for(int j=0; j<y_dim; j++){
+            std::cout<<"[";
+            for(int k=0; k<z_dim; k++){
+                std::cout<<myarr[i][j][k]<<",";
+            }
+            std::cout<<"], ";
+        }
+        std::cout<<"]\n";
+    }
 
-	return 0;
+    test_arr[0][0][0] = 1;
+
+    //We can't simply use delete[] myarr to delete this 3D array.
+    for(int i=0; i<x_dim; i++){
+        for(int j=0; j<y_dim; j++){
+            // std::cout<<"Here!!\n";
+            delete [] myarr[i][j];
+        }
+        delete [] myarr[i];
+    }
+    delete [] myarr;
+
+    for(int i=0; i<x_dim; i++){
+        for(int j=0; j<y_dim; j++){
+            // std::cout<<"Here!!\n";
+            delete [] test_arr[i][j];
+        }
+        delete [] test_arr[i];
+    }
+    delete [] test_arr;
+
+    return 0;
 }
